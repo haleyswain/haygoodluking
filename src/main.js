3bef8fc535a682c3f63dfcee79e09821a7bde707
haley swain <haleyswain@gmail.com>
2021-12-07 21:04:49 -0800

    add GTM

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag-next";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueGtag, {
+    property: {
+      id: "296062679"
+    }
+  });
+
+app.mount("#app");
